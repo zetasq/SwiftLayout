28da35968e1a37f212473d08b61db39ddc0d589a
Zhu Shengqi <zetasq@gmail.com>
2018-02-14 23:43:18 +0800

    Initial Commit

---
+++ SwiftLayout/SwiftLayout.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLayout.h
+//  SwiftLayout
+//
+//  Created by Zhu Shengqi on 14/02/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLayout.
+FOUNDATION_EXPORT double SwiftLayoutVersionNumber;
+
+//! Project version string for SwiftLayout.
+FOUNDATION_EXPORT const unsigned char SwiftLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLayout/PublicHeader.h>
+
+
