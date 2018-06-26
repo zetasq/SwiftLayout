4821f5f594463d9acfaf0f7d9d09c83eeb2211fe
Zhu Shengqi <zetasq@gmail.com>
2018-06-26 22:21:42 +0800

    Add ConstraintGroup to allow grouping constraints generated from LayoutStmt

---
+++ SwiftLayout/Utils/ConstraintGroup.swift
@@ -0,0 +1,39 @@
+//
+//  ConstraintGroup.swift
+//  SwiftLayout
+//
+//  Created by Zhu Shengqi on 2018/6/26.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#if os(macOS)
+import AppKit
+#elseif os(iOS)
+import UIKit
+#endif
+
+public final class ConstraintGroup {
+    
+    private var constraints: [NSLayoutConstraint] = []
+    
+    internal func addConstraint(_ constraint: NSLayoutConstraint) {
+        constraints.append(constraint)
+    }
+    
+    public func activate() {
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    public func deactivate() {
+        NSLayoutConstraint.deactivate(constraints)
+    }
+    
+}
+
+extension LayoutStmt {
+    
+    public func add(to group: ConstraintGroup) {
+        group.addConstraint(self.constraint)
+    }
+    
+}
